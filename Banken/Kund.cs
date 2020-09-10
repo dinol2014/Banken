50da676a9448bd2ee1f6af5fcd3f9de7d7b1a4b9
Linus Jakobsson <robotlinus123@gmail.com>
2020-09-10 10:46:46 +0200

    Add project files.

---
+++ Banken/Kund.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Kund
+    {
+        public string Name;
+        public int Balence;
+        public string ShowCustumerInfo()
+        {
+            return "User name: " + Name + Environment.NewLine + "User balence: " + Balence;
+        }
+    }
+}
---
+++ Banken/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise = SelectMenuItem();
+        }
+
+        private static int SelectMenuItem()
+        {
+            Console.WriteLine("1 : Add a user.");
+            Console.WriteLine("2 : Remove a user.");
+            Console.WriteLine("3 : Show all users.");
+            Console.WriteLine("4 : Show balecne for a user.");
+            Console.WriteLine("5 : Make a insertion for a user.");
+            Console.WriteLine("6 : Make a withdrow for a user.");
+            Console.WriteLine("7 : Close program.");
+
+            string strAwnser = Console.ReadLine();
+            return int.Parse(strAwnser);
+        }
+    }
+}
